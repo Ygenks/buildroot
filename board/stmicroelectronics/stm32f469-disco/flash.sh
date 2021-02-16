f37a1dac40b3737b5872adcbddfcedc6b7198f9f
Ygenks <eugentoo@gmail.com>
2021-02-16 19:29:21 +0300

    Update flash script

--- board/stmicroelectronics/stm32f469-disco/flash.sh
+++ board/stmicroelectronics/stm32f469-disco/flash.sh
@@ -8,23 +8,17 @@ if ! test -d "${OUTPUT_DIR}" ; then
     exit 1
 fi
 
-${OUTPUT_DIR}/host/bin/openocd -f board/stm32f469discovery.cfg \
+${OUTPUT_DIR}/host/bin/openocd -f board/stm32f469i-disco.cfg \
   -c "init" \
   -c "reset init" \
   -c "flash probe 0" \
-  -c "flash info 0" \
+  -c "flash probe 1" \
+  -c "flash probe 2" \
   -c "flash write_image erase ${OUTPUT_DIR}/images/u-boot.bin 0x08000000" \
-  # -c "flash write_image erase ${OUTPUT_DIR}/images/stm32f469-disco.dtb 0x08004000" \
+  -c "flash write_image erase ${OUTPUT_DIR}/images/stm32f469-disco.dtb 0x08040000" \
+  -c "flash write_image erase ${OUTPUT_DIR}/images/xipImage 0x90000000" \
+  -c "flash verify_image ${OUTPUT_DIR}/images/u-boot.bin 0x08000000" \
+  -c "flash verify_image ${OUTPUT_DIR}/images/stm32f469-disco.dtb 0x08040000" \
+  -c "flash verify_image ${OUTPUT_DIR}/images/xipImage 0x90000000" \
   -c "reset run" \
   -c "shutdown"
-
-# ${OUTPUT_DIR}/host/bin/openocd -f board/stm32f469discovery.cfg \
-#   -c "init" \
-#   -c "reset init" \
-#   -c "flash probe 0" \
-#   -c "flash info 0" \
-#   -c "flash write_image erase ${OUTPUT_DIR}/images/stm32f469i-disco.bin 0x08000000" \
-#   -c "flash write_image erase ${OUTPUT_DIR}/images/stm32f469-disco.dtb 0x08004000" \
-#   -c "flash write_image erase ${OUTPUT_DIR}/images/xipImage 0x08008000" \
-#   -c "reset run" \
-#   -c "shutdown"
