d6c2ada55fecd8dc025f9cddcbe2c933e34a78bc
Ygenks <eugentoo@gmail.com>
2021-03-24 17:59:21 +0300

    Boot image from sd-card, enable hw random generator

--- board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
+++ board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
@@ -6,3 +6,10 @@ sed -i '/^devpts/d' ${TARGET_DIR}/etc/fstab
 # Kernel is built without network support
 rm -f ${TARGET_DIR}/etc/init.d/S40network
 rm -rf ${TARGET_DIR}/etc/network/
+
+local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
+local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+support/scripts/genimage.sh -c ${GENIMAGE_CFG}
+
+rm -f ${GENIMAGE_CFG}
