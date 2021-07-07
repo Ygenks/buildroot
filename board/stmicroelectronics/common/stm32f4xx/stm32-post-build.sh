c6226626b59e0f0ccbe4f5ea4e5dbcbaa51136ec
Ygenks <eugentoo@gmail.com>
2021-07-08 01:32:02 +0300

    board/stm32f469-disco: update board configuration
    Update STM32F469-disco configuration files to operate with new kernel.
    
    Result of make tinyconfig was taken as a starting point to fit kernel
    into flash memory.
    Current setup fits in 2MB on-chip flash memory with rootfs
    offloaded to MMC card.
    
    Kernel features enabled:
        - MMC card driver
        - DRM MIPI-DSI drivers
        - Random number generator
    Fixes:
        - Move kernel to new flash bank due to growth of dtb size
        - Fix kernel start address in bootloader
        - Remove outdated path which doesn't affect normal operation mode
    
    Signed-off-by: Yauheni Saldatsenka <eugentoo@gmail.com>

--- board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
+++ board/stmicroelectronics/common/stm32f4xx/stm32-post-build.sh
@@ -6,3 +6,10 @@ sed -i '/^devpts/d' ${TARGET_DIR}/etc/fstab
 # Kernel is built without network support
 rm -f ${TARGET_DIR}/etc/init.d/S40network
 rm -rf ${TARGET_DIR}/etc/network/
+
+GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+support/scripts/genimage.sh -c ${GENIMAGE_CFG}
+
+rm -f ${GENIMAGE_CFG}
